2d3d5d5c8c23cba9e72c6b583639ce0ed879f9a2
anuj9631 <anuj98426@gmail.com>
2025-08-29 16:17:28 +0530

    API setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/NewsBoard"
 
 
 const App = () => {
   return (
     <div>
 <Navbar />
+<NewsBoard />
     </div>
   )
 }
--- src/Components/Newsitem.jsx
+++ src/Components/Newsitem.jsx
@@ -1,8 +1,18 @@
 
 
-const Newsitem = () => {
+const Newsitem = ({title,description,src,url}) => {
   return (
-    <div>Newsitem</div>
+    <div>
+
+      <div className="card" style={{maxWidth:"345px"}}>
+  <img src={src} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
   )
 }
 
