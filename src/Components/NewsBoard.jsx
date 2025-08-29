c147d63539b4e25778711d71bd397a191bd3f619
anuj9631 <anuj98426@gmail.com>
2025-08-29 16:53:06 +0530

    Project done

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import Newsitem from "./Newsitem";
 
 
-const NewsBoard = () => {
+const NewsBoard = ({category}) => {
 
   const [articles, setArticles] = useState([]);
 
   useEffect(()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
 
     fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-  },[])
+  },[category])
 
   return (
     <div>
